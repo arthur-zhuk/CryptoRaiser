64c4dd930871264eb1f275339c1c554ebccb852f
Arthur Zhuk <arthurzhuk@gmail.com>
2018-07-02 12:35:56 -0700

    next build

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const { createServer } = require('http');
 const next = require('next');
-
-
 const app = next({
   dev: process.env.NODE_ENV !== 'production'
 });
-
 const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
 
